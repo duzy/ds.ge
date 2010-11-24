a76c607345b5e1cb9020632a21c473eebc44696c
Duzy Chan <code@duzy.info>
2010-11-24 12:13:26 +0800

    refactor build script

--- smart.mk
+++ smart.mk
@@ -1,6 +1,8 @@
 #	Copyright Duzy Chan <code@duzy.info>
 #
 
+NULL :=
+
 $(call sm-new-module, dsge, shared)
 
 ds.ge.dir := $(sm.this.dir)
@@ -17,11 +19,12 @@ sm.this.compile.options := \
   -DDS_DEBUG_LEVEL=3 \
   -DDS_LOG_LEVEL=3 \
   -DDS_BUILDING_GRAPHICS=1 \
+  $(NULL)
 
 sm.this.link.options.infile := true
 sm.this.link.options := \
-  -Wl,--no-undefined \
-  -Wl,--rpath,$(ds.ge.dir)/$(strip $(ds.third.dir.lib))
+  -Wl,--rpath,$(ds.ge.dir)/$(strip $(ds.third.dir.lib)) \
+  $(NULL)
 
 sm.this.includes := \
   $(ds.ge.dir)/include \
@@ -29,26 +32,23 @@ sm.this.includes := \
   $(ds.third.dir.inc) \
   $(ds.third.dir.inc)/zlib \
   $(ds.third.dir.inc)/libpng \
-  $(ds.third.dir.inc)/skia/config \
   $(ds.third.boost.dir) \
-  $(ds.third.boost.geometry)
+  $(ds.third.boost.geometry) \
+  $(NULL)
 
 sm.this.sources := \
   $(wildcard src/*.cpp) \
   $(wildcard src/gil/*.cpp) \
+  $(NULL)
 
 sm.this.libdirs := \
-  -L$(ds.third.dir.lib) \
-  -L$(ds.third.boost.dir.lib)
+  $(ds.third.dir.lib) \
+  $(ds.third.boost.dir.lib) \
+  $(NULL)
 
 $(call sm-check-not-empty, ds.third.skia.libname)
 $(call sm-check-not-empty, ds.third.libpng.libname)
 $(call sm-check-not-empty, ds.third.freetype.libname)
-sm.this.libs += \
-  $(ds.third.skia.libname) \
-  $(ds.third.libpng.libname) \
-  $(ds.third.freetype.libname) \
-  $(ds.third.zlib.libname) \
 
 #  $(call ds.third.boost.use, system) \
 
@@ -59,6 +59,18 @@ ifeq ($(ds.ge.base),ds)
 else
 ifeq ($(ds.ge.base),skia)
   sm.this.sources += $(wildcard src/skia/*.cpp)
+  sm.this.includes += \
+    $(ds.third.dir.inc)/skia/config \
+    $(NULL)
+  sm.this.libs += \
+    $(ds.third.freetype.libname) \
+    $(ds.third.libpng.libname) \
+    $(ds.third.skia.libname) \
+    $(ds.third.zlib.libname) \
+    $(NULL)
+else
+ifeq ($(ds.ge.base),cairo)
+  sm.this.sources += $(wildcard src/cairo/*.cpp)
 else
 ifeq ($(ds.ge.base),qt)
   QT := $(if $(sm.os.name.linux),/usr/local/Trolltech/Qt-4.7.1,c:/Qt/4.7.0)
@@ -71,31 +83,30 @@ ifeq ($(ds.ge.base),qt)
   sm.this.includes += $(QT)/include
   sm.this.link.options += -Wl,--rpath,$(if $(sm.os.name.win32),$(QT)/bin,$(QT)/lib)
 endif#qt-based
+endif#cairo-based
 endif#skia-based
 endif#ds-based
 
 ifeq ($(sm.os.name),linux)
   sm.this.libs += pthread
   sm.this.targets += $(sm.out.lib)/libdsge.so
-  #$(sm.out.lib)/libdsge.so : $(sm.out.lib) $(sm.var.dsge.targets)
-  $(sm.out.lib)/libdsge.so : $(sm.out.lib) $(sm.out.bin)/dsge.so
-	$(call sm.tool.common.ln,$(sm.top)/$(sm.out.bin)/dsge.so,$@)
+  $(sm.out.lib)/libdsge.so : $(sm.out.lib) $(sm.var.dsge.module_targets)
+	$(call sm.tool.common.ln,$(sm.top)/$(sm.var.dsge.module_targets),$@)
 else
 ifeq ($(sm.os.name),win32)
   sm.this.compile.options += -mwindows
   sm.this.link.options += -mwindows \
     -Wl,--out-implib,$(sm.out.lib)/libdsge.a \
     -Wl,--enable-runtime-pseudo-reloc \
-    -Wl,--enable-auto-import
-  sm.this.depends += $(sm.out.lib)/libdsge.a
-  $(sm.out.lib)/libdsge.a : $(sm.out.lib) $(sm.this.targets)
-  #sm.this.depends += $(sm.out.lib)/libdsge.a
+    -Wl,--enable-auto-import \
+    $(NULL)
+  sm.this.targets += $(sm.out.lib)/libdsge.a
+  $(sm.out.lib)/libdsge.a : $(sm.out.lib) $(sm.var.dsge.module_targets)
 
-  sm.this.depends += \
+  thirdlibs += \
     $(ds.ge.dir.bin)/$(ds.third.libpng.libname).so \
     $(ds.ge.dir.bin)/$(ds.third.libxml.libname).so
-  $(ds.ge.dir.bin)/$(ds.third.libpng.libname).so: $(ds.third.dir.bin)/$(ds.third.libpng.libname).so ; @cp -vf $< $@
-  $(ds.ge.dir.bin)/$(ds.third.libxml.libname).so: $(ds.third.dir.bin)/$(ds.third.libxml.libname).so ; @cp -vf $< $@
+  $(call sm-copy-files,$(thirdlibs),$(ds.ge.dir.bin))
 else
 ifeq ($(sm.os.name),mac)
   # ...
