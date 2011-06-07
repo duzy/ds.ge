e9870076b591e946641e900471a3ce7d31ade4b1
Duzy Chan <code@duzy.info>
2011-06-07 18:18:46 +0800

    refactored png_io.hpp and add libpng error handler to it

--- include/ds/graphics/gil/png_io.hpp
+++ include/ds/graphics/gil/png_io.hpp
@@ -4,7 +4,7 @@
 #	include <cstdio> //!< for fopen, fclose in gil/extension/io on MinGW
 extern "C" {
 #	include <png.h>
-#   if PNG_LIBPNG_VER_MAJOR==1 && PNG_LIBPNG_VER_MINOR==4 && PNG_LIBPNG_VER_RELEASE>=3
+#   if PNG_LIBPNG_VER_MAJOR==1 && PNG_LIBPNG_VER_MINOR>=4
 #      define png_infopp_NULL NULL /* for png_dynamic_io.hpp, libpng-1.4.x don't have */
 #      define int_p_NULL NULL /* for png_dynamic_io.hpp, libpng-1.4.x don't have */
 #   endif
@@ -14,28 +14,44 @@ extern "C" {
 #	include <boost/gil/extension/io/png_io.hpp>
 #	include <boost/gil/extension/io/png_dynamic_io.hpp>
 #	include <istream>
-#   include <cxxabi.h>
-/*
-  int cxa_status;
-  std::clog<<__FILE__<<":"<<__LINE__<<":"<<abi::__cxa_demangle(typeid(view).name(), 0, 0, &cxa_status)<<std::endl;
-*/
+#   include <ds/cxxabi.hpp>
 
 namespace ds { namespace graphics { namespace gil {
+
+      struct png_io
+      {
+      protected:
+        static void error_handler(png_structp ptr, png_const_charp msg)
+        {
+          //png_io * self((png_reader*)png_get_error_ptr(ptr));
+          dsE("png: "<<msg);
+        }
+
+        static void warn_handler(png_structp ptr, png_const_charp msg)
+        {
+          //png_io * self((png_reader*)png_get_error_ptr(ptr));
+          dsL("png: "<<msg);
+        }
+      };
 
       /**
        *  @brief Read PNG image from std::istream
        */
-      struct png_reader
+      struct png_reader : png_io
       {
         static bool check( std::istream & is )
         {
           const std::size_t sigSize( boost::gil::detail::PNG_BYTES_TO_CHECK );
+
           char buf[ sigSize ];
           is.read( buf, sigSize );
+
           if ( is.gcount() != sigSize )
-            { dsE("png_check_validity: failed to read file"); return false; }
+            { dsE("png_reader::check: failed to read file"); return false; }
+
           if ( png_sig_cmp((png_bytep)buf, (png_size_t)0, sigSize) != 0 )
-            { dsE("png_check_validity: invalid png file"); return false; }
+            { dsE("png_reader::check: invalid png file"); return false; }
+
           return true; //!< good png stream
         }
 
@@ -45,28 +61,36 @@ namespace ds { namespace graphics { namespace gil {
           , _is   ( is )
         {
           const std::size_t sigSize( boost::gil::detail::PNG_BYTES_TO_CHECK );
-          if ( doCheck && !check(_is) ) { dsE("png_reader::check: bad png stream"); }
-          _png = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
-          if ( _png == NULL ) { dsE("failed to call png_create_read_struct()"); return; }
+          if ( doCheck && !check(_is) )
+            { dsE("png_reader::check: bad png stream"); return; }
+
+          _png = png_create_read_struct(PNG_LIBPNG_VER_STRING, this, error_handler, warn_handler);
+          if ( _png == NULL )
+            { dsE("failed png_create_read_struct(): "<<PNG_LIBPNG_VER_STRING); return; }
+
           _info = png_create_info_struct( _png );
-          if ( _info == NULL ) {
-            png_destroy_read_struct(&_png,png_infopp_NULL,png_infopp_NULL);
-            dsE("failed to call png_create_info_struct()"); return;
-          }
-          if (setjmp(png_jmpbuf(_png))) {
-            png_destroy_read_struct(&_png,&_info,png_infopp_NULL);
-            dsE("failed to call setjmp()"); return;
-          }
-          png_set_read_fn(_png,this,&png_reader::read_data); //png_init_io(_png,get());
-          png_set_sig_bytes(_png,sigSize);
-          png_read_info(_png,_info);
+          if ( _info == NULL )
+            {
+              png_destroy_read_struct(&_png, NULL, NULL);
+              dsE("failed png_create_info_struct()"); return;
+            }
+
+          if (setjmp(png_jmpbuf(_png)))
+            {
+              png_destroy_read_struct(&_png, &_info, NULL);
+              dsE("failed to call setjmp()"); return;
+            }
+
+          png_set_read_fn(_png, this, &png_reader::read_data); //png_init_io(_png, get());
+          png_set_sig_bytes(_png, sigSize);
+          png_read_info(_png, _info);
           if (boost::gil::little_endian() && 8 < png_get_bit_depth(_png,_info))
             png_set_swap(_png);
         }
 
         virtual ~png_reader()
         {
-          png_destroy_read_struct(&_png,&_info,png_infopp_NULL);
+          png_destroy_read_struct(&_png, &_info, png_infopp_NULL);
         }
 
         boost::gil::point2<std::ptrdiff_t> get_dimensions()
@@ -88,8 +112,8 @@ namespace ds { namespace graphics { namespace gil {
         void read(const boost::gil::image_view<Locator<Iterator<boost::gil::packed_pixel<VT,PF,Layout>*> > > & view)
         {
           // TODO: read packed pixels
-          int cxa_status;
-          std::clog<<__FILE__<<":"<<__LINE__<<":TODO: read "<<abi::__cxa_demangle(typeid(view).name(), 0, 0, &cxa_status)<<std::endl;
+          boost::shared_ptr<char> s = ds::cxxabi::demangle(typeid(view).name());
+          std::clog<<__FILE__<<":"<<__LINE__<<":TODO: read "<<s.get()<<std::endl;
         }
 
         template <typename Locator>
@@ -111,7 +135,7 @@ namespace ds { namespace graphics { namespace gil {
 
           png_uint_32 width, height;
           int bit_depth, color_type, interlace_type;
-          png_get_IHDR(_png,_info, &width, &height,&bit_depth,&color_type,&interlace_type, int_p_NULL, int_p_NULL);
+          png_get_IHDR(_png,_info, &width, &height, &bit_depth, &color_type, &interlace_type, int_p_NULL, int_p_NULL);
           io_error_if(((png_uint_32)view.width()!=width || (png_uint_32)view.height()!= height),
                       "png_read_view: input view size does not match PNG file size");
 
@@ -120,9 +144,9 @@ namespace ds { namespace graphics { namespace gil {
             io_error("png_read_view: input view type is incompatible with the image type");
 
           std::vector<pixel_t> row(width);
-          for(png_uint_32 y=0;y<height;++y) {
+          for (png_uint_32 y=0; y<height; ++y) {
             png_read_row(_png,(png_bytep)&row.front(),NULL);
-            std::copy(row.begin(),row.end(),view.row_begin(y));
+            std::copy(row.begin(), row.end(), view.row_begin(y));
           }
           png_read_end(_png,NULL);
         }
@@ -176,7 +200,7 @@ namespace ds { namespace graphics { namespace gil {
         std::istream &  _is;
       };//struct png_reader
 
-      struct png_writer
+      struct png_writer : png_io
       {
         png_writer( std::ostream & os )
           : _png    ( NULL )
@@ -185,21 +209,27 @@ namespace ds { namespace graphics { namespace gil {
         {
           using boost::gil::io_error;
           using boost::gil::io_error_if;
-          _png = png_create_write_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
+          _png = png_create_write_struct(PNG_LIBPNG_VER_STRING, this, error_handler, warn_handler);
           io_error_if(!_png,"png_write_initialize: fail to call png_create_write_struct()");
+
           _info = png_create_info_struct(_png);
-          if (!_info) {
-            png_destroy_write_struct(&_png,png_infopp_NULL);
-            io_error("png_write_initialize: fail to call png_create_info_struct()");
-          }
-          if (setjmp(png_jmpbuf(_png))) {
-            png_destroy_write_struct(&_png, &_info);
-            io_error("png_write_initialize: fail to call setjmp(png_jmpbuf())");
-          }
+          if (!_info)
+            {
+              png_destroy_write_struct(&_png,png_infopp_NULL);
+              io_error("png_write_initialize: fail to call png_create_info_struct()");
+            }
+
+          if (setjmp(png_jmpbuf(_png)))
+            {
+              png_destroy_write_struct(&_png, &_info);
+              io_error("png_write_initialize: fail to call setjmp(png_jmpbuf())");
+            }
+
           png_set_write_fn(_png, this, &png_writer::write_data, &png_writer::flush_data); //png_init_io(_png,get());
         }
 
-        ~png_writer() {
+        ~png_writer()
+        {
           png_destroy_write_struct(&_png,&_info);
         }
 
@@ -216,8 +246,8 @@ namespace ds { namespace graphics { namespace gil {
         void write(const boost::gil::image_view<Locator<Iterator<boost::gil::packed_pixel<VT,PF,Layout>*> > > & view)
         {
           // TODO: write packed pixels
-          int cxa_status;
-          std::clog<<__FILE__<<":"<<__LINE__<<":TODO: write "<<abi::__cxa_demangle(typeid(view).name(), 0, 0, &cxa_status)<<std::endl;
+          boost::shared_ptr<char> s = ds::cxxabi::demangle(typeid(view).name());
+          std::clog<<__FILE__<<":"<<__LINE__<<":TODO: write "<<s.get()<<std::endl;
         }
 
         template <typename Locator>
